52618fc1ccc8ed16681f0f3e0f4bcb18620f5a05
W. Christohper Carleton <ccarleton@protonmail.com>
2021-06-08 13:50:04 +0200

    cleaning

--- R/approx_c14.R
+++
@@ -1,8 +0,0 @@
-approx_c14 <- function(x,t1,t2,r){
-    n <- length(x)
-    funs <- lapply(x,approxfun)
-    y_list <- lapply(1:n,function(j)funs[[j]](seq(t1,t2,r)))
-    y_mat <- do.call(cbind,y_list)
-    y_mat[which(is.na(y_mat))] <- 0
-    return(y_mat)
-}
